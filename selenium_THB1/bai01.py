59dc3d94d55fa8a692ac2c87d02e96fb15a271e9
ngthikieuanh <piiip5972@gmail.com>
2025-12-04 13:32:39 +0700

    project1

---
+++ selenium_THB1/bai01.py
@@ -0,0 +1,27 @@
+from pygments.formatters.html import webify
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+# Khoi tao
+driver = webdriver.Chrome()
+
+#mo trang
+url = "https://en.wikipedia.org/wiki/List_of_painters_by_name"
+driver.get(url)
+
+#mo toan man hinh
+driver.maximize_window()
+
+#doi 2 giay
+time.sleep(2)
+
+#lay tat ca the <a>
+tags = driver.find_elements(By.TAG_NAME, "a")
+#tao ds cac l.ket
+links = [tag.get_attribute("href") for tag in tags]
+
+for link in links:
+    print(link)
+
+driver.quit()
